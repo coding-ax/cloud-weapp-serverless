21e6e28dbc05228526f6feaf06413ad349ab2941
whutfaker <1312976124@qq.com>
2020-08-15 17:38:23 +0800

    Initial Commit

---
+++ miniprogram/components/icon-self/icon-self.js
@@ -0,0 +1,29 @@
+// components/icon-self/icon-self.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        src: {
+            type: String,
+        },
+        size: {
+            type: Number,
+            value:32
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
